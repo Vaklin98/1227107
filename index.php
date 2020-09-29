fbb90e102bcf5de35551eb054422086c506d54b6
NFSRacers <svetoslav@hadzhitodev.com>
2020-09-29 08:55:59 +0300

    upr3

--- index.php
+++ index.php
@@ -1,26 +1,76 @@
 <?php 
-echo "Hello world ";
-$username ="Vaklin98";
-echo $username."<br>";
-echo"my name is $username  <br>";
-$age = 12;
-echo $age;
-$istrue = true;
-echo $istrue."<br>";
-$username_Array = ["vaklin","danBoy"];
-echo $username_Array[0];
- $username_Asoc_Array = ["username"=>"Vaklin","h"=>"3"];
- echo"<br>";
-echo $username_Asoc_Array["h"];
-echo"<br>";
-for($i =0;$i<2;$i++) 
+// echo "Hello world ";
+// $username ="Vaklin98";
+// echo $username."<br>";
+// echo"my name is $username  <br>";
+// $age = 12;
+// echo $age;
+// $istrue = true;
+// echo $istrue."<br>";
+// $username_Array = ["vaklin","danBoy"];
+// echo $username_Array[0];
+//  $username_Asoc_Array = ["username"=>"Vaklin","h"=>"3"];
+//  echo"<br>";
+// echo $username_Asoc_Array["h"];
+// echo"<br>";
+// for($i =0;$i<2;$i++) 
+// {
+//  echo $username_Array[$i]; 
+//  echo"<br>";  
+
+// }
+// foreach($username_Asoc_Array as $key => $dsad) 
+// {
+//     echo $key."<br>";
+//     echo $dsad."<br>";
+// } 
+// $username = "Oliviq";
+// if($username=="Oliviq")
+// {
+//     echo"Oliviq e zle";
+// }
+// elseif(1<1)
+// {
+//     echo "oliviq  e topa";
+// }
+// else
+// {
+//     echo"Pak e zle";
+// }
+
+// $username ="Oliviq";
+
+// switch($username)
+// {
+//     case $username =="Oliviya":
+//         echo"Neshto";
+//             break;
+//     case $username == "Oliviq":
+    
+//         echo" Top fen";
+//             break;
+//     default:
+
+//         echo" default messege";
+// }
+
+
+// function printName($username)
+// {
+// echo $username;
+// }
+
+
+function isThisElmira($username)
 {
- echo $username_Array[$i]; 
- echo"<br>";  
+    if($username=="Elmira")
+    {
+       echo"This is Elmira";
+    }
+    else
+    {
+        echo"Its Oliviqqqq!";
+    }
 
 }
-foreach($username_Asoc_Array as $key => $dsad) 
-{
-    echo $key."<br>";
-    echo $dsad."<br>";
-}
+isThisElmira("Elmira");
